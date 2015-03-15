d805e6932989e2b73588c67860f3c41dc7fe23b3
Park June Chul <pjc0247@naver.com>
2015-03-15 16:28:12 +0900

    Create job.rb

---
+++ src/job.rb
@@ -0,0 +1,24 @@
+require 'mail'
+require 'json'
+
+def run cmd, *args
+  IO.popen([cmd, *args]) do |io|
+    return io.read
+  end
+end
+
+result = JSON.parse(run("casperjs", "push.js"))
+
+exit if result["alert"] == false
+
+mail = Mail.new do
+  from     'pjc0247@naver.com'
+  to       'pjc0247@nhnnext.org'
+  subject  '[UCRAFT] 공격 알림'
+  body '공격ㄱ격격옴'
+
+  add_file 'capture.png'
+end
+
+mail.delivery_method :sendmail, :location => "./sendmail"
+mail.deliver
